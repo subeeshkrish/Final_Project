9ddf80ac2c731468387df591dd4d8e5557eea818
Subishkumar <104686081+subeeshkrish@users.noreply.github.com>
2023-04-06 11:36:58 +0530

    Delete import boto3.py

--- import boto3.py
+++
@@ -1,4 +0,0 @@
-import boto3
-s3_client = boto3.client('s3')
-response = s3_client.upload_file(r'D:\subishkrish\split\part2\randm.txt.part2', 'private2proj', 'part2/randm.txt.part2')
-response = s3_client.upload_file(r'D:\subishkrish\split\part1\randm.txt.part1', 'my-project02', 'part1/randm.txt.part1')
